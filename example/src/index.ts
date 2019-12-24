75e60cbad60471d2564122886d0ab14a366672e5
QuinnCiccoretti <quinnc423@gmail.com>
2019-12-23 20:59:59 -0500

    attempting to restructure everything

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,22 +3,6 @@ import * as THREE from 'three';
 import {SceneManager} from 'scenemanager';
 import {isVREnabled, addControls, updateControls} from 'controlmanager';
 import {parseDotOutput} from 'terra-parse';
-//make all the objects
-var terraform_json:any = parseDotOutput();
-console.log(terraform_json);
-
-var scene = new SceneManager(terraform_json);
-var color1picker = document.getElementById("color1");
-color1picker.addEventListener("change", function(event:any){
-  var color = event.target.value;
-  scene.updateSkyColor(color);
-});
-var color2picker = document.getElementById("color2");
-color2picker.addEventListener("change", function(event:any){
-  var color = event.target.value;
-  scene.updateGroundColor(color);
-});
-
 var renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -26,21 +10,46 @@ document.body.appendChild( renderer.domElement );
 
 var controls:any;
 var vrDisplay:any;
-var blocker = document.getElementById("blocker");
+var scene:SceneManager|null;
 var startbutton = document.getElementById("startButton");
-addControls(scene, scene.camera, blocker,startbutton).then(
-	function(){
-  	if(isVREnabled()){
-  		vrDisplay.requestAnimationFrame(vrAnimate);
-  	}
-  	else{
-  		requestAnimationFrame(desktopAnimate);
-  	}
+startbutton.addEventListener("click",renderScene);
+renderScene();
+function renderScene(){
+  //dispose of old scene if defined
+  if(scene){
+    scene.dispose();
   }
-).catch(function(error){
-	console.log(error);
-});
+  //make all the objects
+  var text = (<HTMLInputElement>document.getElementById('textbox')).value;
+  var terraform_json:any = parseDotOutput(text);
 
+  scene = new SceneManager(terraform_json);
+  var color1picker = document.getElementById("color1");
+  color1picker.addEventListener("change", function(event:any){
+    var color = event.target.value;
+    scene.updateSkyColor(color);
+  });
+  var color2picker = document.getElementById("color2");
+  color2picker.addEventListener("change", function(event:any){
+    var color = event.target.value;
+    scene.updateGroundColor(color);
+  });
+
+  
+  var blocker = document.getElementById("blocker");
+  addControls(scene, scene.camera, blocker,startbutton).then(
+  	function(){
+    	if(isVREnabled()){
+    		vrDisplay.requestAnimationFrame(vrAnimate);
+    	}
+    	else{
+    		requestAnimationFrame(desktopAnimate);
+    	}
+    }
+  ).catch(function(error){
+  	console.log(error);
+  });
+}
 
 
 function vrAnimate(){
